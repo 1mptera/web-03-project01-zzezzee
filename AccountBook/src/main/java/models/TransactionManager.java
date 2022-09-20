82ff80fcb1b69f2bee8624b1e0d583aa8c6508a8
zzezzee <GitHub zzezzee@naver.com>
2022-09-20 23:13:28 +0900

    git ignore

--- AccountBook/src/main/java/models/TransactionManager.java
+++ AccountBook/src/main/java/models/TransactionManager.java
@@ -1,2 +1,17 @@
-package models;public class TransactionManager {
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TransactionManager {
+    public List<Transaction> transactions = new ArrayList<>();
+
+    public void add(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public List<Transaction> transaction() {
+        return transactions;
+    }
 }
--- AccountBook/src/test/java/models/TransactionManagerTest.java
+++ AccountBook/src/test/java/models/TransactionManagerTest.java
@@ -1,4 +1,23 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TransactionManagerTest {
-  
+
+    @Test
+    void addTransaction() {
+        TransactionManager transactionManager = new TransactionManager();
+
+        transactionManager.add(new Transaction(
+                "2022-09-20",
+                "수입",
+                "현금",
+                10000,
+                "용돈"
+        ));
+
+        assertEquals(1, transactionManager.transactions.size());
+    }
 }
