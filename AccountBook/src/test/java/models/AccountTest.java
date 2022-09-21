c05f03ce90ece5bf8d3a16de46b0db23673e27f8
zzezzee <GitHub zzezzee@naver.com>
2022-09-21 22:41:03 +0900

    Make LedgerService
    Make LedgerService domain model

--- AccountBook/src/test/java/models/AccountTest.java
+++ AccountBook/src/test/java/models/AccountTest.java
@@ -13,4 +13,23 @@ class AccountTest {
         assertEquals("통장1", account.name());
         assertEquals(1000, account.amount());
     }
+
+    @Test
+    void receive() {
+        Account account = new Account("재원통장", 1000);
+
+        account.receive(1000);
+
+        assertEquals(2000, account.amount());
+    }
+
+    @Test
+    void spend() {
+        Account account = new Account("재원통장", 1000);
+
+        account.spend(1000);
+
+        assertEquals(0, account.amount());
+    }
+
 }
